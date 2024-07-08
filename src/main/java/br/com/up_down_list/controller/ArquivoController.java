e9c0dc450ca354747af711c68c867e6445803e84
Wesley Rosa <wesleymrosa@gmail.com>
2024-07-08 15:42:42 -0300

    1) Inserido o ajuste para salvar apenas arquivos do tipo .pdf. 2) Iniciado o projeto Angular do projeto.

--- src/main/java/br/com/up_down_list/controller/ArquivoController.java
+++ src/main/java/br/com/up_down_list/controller/ArquivoController.java
@@ -3,6 +3,7 @@ package br.com.up_down_list.controller;
 import br.com.up_down_list.dto.ArquivoRetornoDTO;
 import br.com.up_down_list.entity.ArquivoEntity;
 import br.com.up_down_list.service.ArquivoServise;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
@@ -14,31 +15,32 @@ import java.io.IOException;
 import java.util.List;
 
 @Slf4j
-
 @RestController
-@CrossOrigin (origins = "*")
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/arquivos")
+@RequiredArgsConstructor
 public class ArquivoController {
 
     private final ArquivoServise arquivoServise;
 
-    public ArquivoController(ArquivoServise arquivoServise) {
-        this.arquivoServise = arquivoServise;
-    }
-
-
     @PostMapping
     public ResponseEntity<String> salvar(@RequestParam("file") MultipartFile file) throws IOException {
-        try {
-            arquivoServise.salvarArquivo(file);
-            System.out.println("O arquivo:       " + file.getOriginalFilename() + "       foi salvo com sucesso !");
-            log.info("O arquivo:       " + file.getOriginalFilename() + "       foi salvo com sucesso !");
-            return ResponseEntity.status(HttpStatus.CREATED).body("O arquivo: [" + file.getOriginalFilename() + "] foi salvo com sucesso !");
-        } catch (IOException e) {
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao subir o arquivo.");
 
+        if (file.getOriginalFilename().toLowerCase().endsWith(".pdf")) {
+            try {
+                arquivoServise.salvarArquivo(file);
+                //System.out.println("Arquivo .pdf salvo com sucesso ! ");
+                System.out.println("O arquivo:       " + file.getOriginalFilename() + "       foi salvo com sucesso !");
+                log.info("O arquivo:       " + file.getOriginalFilename() + "       foi salvo com sucesso !");
+                return ResponseEntity.status(HttpStatus.CREATED).body("Arquivo do tipo .pdf salvo com sucesso ! \nO arquivo: [" + file.getOriginalFilename() + "] foi salvo com sucesso !");
+            } catch (IOException e) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Erro ao subir o arquivo.");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("Esse arquivo não é foi salvo ! \nSomente arquivos do tipo .pdf são permitidos neste sistema !");
         }
     }
+
 
     @GetMapping
     public ResponseEntity<List<ArquivoEntity>> listar() {
@@ -50,9 +52,7 @@ public class ArquivoController {
     public ResponseEntity<Resource> downloadFile(@PathVariable Long id) {
         try {
             ArquivoRetornoDTO arquivoRetornoDTO = arquivoServise.downloadArquivo(id);
-            return ResponseEntity.ok()
-                    .header("Content-Disposition", "attachment; filename=\"" + arquivoRetornoDTO.getNome().concat(".pdf") + "\"")
-                    .body(arquivoRetornoDTO.getFile());
+            return ResponseEntity.ok().header("Content-Disposition", "attachment; filename=\"" + arquivoRetornoDTO.getNome().concat(".pdf") + "\"").body(arquivoRetornoDTO.getFile());
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         }
